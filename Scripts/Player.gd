eafb37ee0c0d441b6b7eb7ab7cc9c41e8ba08f79
Kipras <kiprasjanuska0@gmail.com>
2019-10-07 18:48:42 -0500

    fixed some stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var SPEED = 50
+var motion = Vector2()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		motion.x += SPEED
+	
+	if Input.is_action_pressed("ui_left"):
+		motion.x -= SPEED
