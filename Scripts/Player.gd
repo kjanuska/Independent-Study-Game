632ee6a3d235655358500e782bf19d4f400e33a4
Kipras <kiprasjanuska0@gmail.com>
2019-10-07 19:01:36 -0500

    basic movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,22 @@
-extends Node2D
+extends KinematicBody2D
 
-var SPEED = 50
+var SPEED = 200
 var motion = Vector2()
 
-func _process(delta):
+func _physics_process(delta):
+	motion.x = 0
+	motion.y = 0
+	
 	if Input.is_action_pressed("ui_right"):
-		motion.x += SPEED
+		motion.x = SPEED
 	
 	if Input.is_action_pressed("ui_left"):
-		motion.x -= SPEED
+		motion.x = -SPEED
+	
+	if Input.is_action_pressed("ui_up"):
+		motion.y = -SPEED
+	
+	if Input.is_action_pressed("ui_down"):
+		motion.y = SPEED
+		
+	move_and_slide(motion)
