098f3ba75cb48f3c26d2d176a7ac2b24ae688597
Kipras <kiprasjanuska0@gmail.com>
2019-10-09 19:55:37 -0500

    Revert "Revert "started dash functionality""
    This reverts commit e03b065ebf4ef28880fadfb089b0f8b11f4cdf56.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,8 +2,18 @@ extends KinematicBody2D
 
 var SPEED = 300
 var motion = Vector2()
+var moving = false
+var can_move = true
+var can_dash = true
+var player
+
+func _ready():
+	player = get_node(".")
 
 func _physics_process(delta):
+	motion.x = 0
+	motion.y = 0
+	moving = false
 	
 	_get_input()
 
@@ -13,22 +23,39 @@ func _physics_process(delta):
 		get_tree().quit()
 
 
-
 func _get_input():
-	motion.x = 0
-	motion.y = 0
-	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-	
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-	
-	if Input.is_action_pressed("ui_up"):
-		motion.y = -SPEED
-	
-	if Input.is_action_pressed("ui_down"):
-		motion.y = SPEED
+	if can_move:
+		if Input.is_action_pressed("ui_right"):
+			motion.x = SPEED
+			moving = true
+		
+		if Input.is_action_pressed("ui_left"):
+			motion.x = -SPEED
+			moving = true
+		
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -SPEED
+			moving = true
+		
+		if Input.is_action_pressed("ui_down"):
+			motion.y = SPEED
+			moving = true
 	
 	if Input.is_action_just_pressed("space"):
-		print("some mechanic")
+		if moving && can_dash:
+			can_move = false
+			can_dash = false
+			player.visible = not player.visible
+			var target_position = player.get_position() + ((motion.normalized()) * 150)
+#			trying to get the player to be animated from current position to target position instead of just teleporting
+			if ((player.get_position().x < target_position.x) && (player.get_position().y < target_position.y)):
+				pass
+			$DashTimer.start()
+
+func _on_DashTimer_timeout():
+	player.visible = not player.visible
+	can_move = true
+	$DashCooldown.start()
+
+func _on_DashCooldown_timeout():
+	can_dash = true
