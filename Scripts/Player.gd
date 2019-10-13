acd9994eaa5f79fbcd440e66de3712add818f8bb
Kipras <kiprasjanuska0@gmail.com>
2019-10-13 16:41:40 -0500

    equip and notes
    added feature that allows you to equip melee and ranged weapons (no sword attack yet). added clarification to code to make it easier to understand.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,40 @@
 extends KinematicBody2D
 
+#speed that the player travels at
 var SPEED = 300
+#player direction vector
 var motion = Vector2()
+#is the player currently moving using the WASD keys
 var moving = false
+#is the player in the process of dashing
 var dashing = false
+#is the player not dashing (can't move while dashing)
 var can_move = true
-var can_dash = true
+#used in _ready to get parent node
 var player
 
-#projectile
-var shoot_speed = 20
-var bullet_load = preload("res://Scenes/Projectile.tscn")
+#store preloaded scene as var
+var bullet_load = preload("res://Scenes/Gun/Bullet.tscn")
+#used later to assign to loaded ranged weapon scene
+var ranged
+#stores preloaded gun as var
+var gun_load = preload("res://Scenes/Gun/Gun.tscn")
 
+#used later to assign to loaded melee weapon scene
+var melee
+#store preloaded sword as var
+var sword_load = preload("res://Scenes/Sword.tscn")
+
+#0 = ranged
+#1 = melee
+var current_weapon = 0
+
+#equip gun as soon as the player is loaded
 func _ready():
 	player = get_node(".")
+	_equip_ranged()
 
+#check for movement input, dash, move character
 func _physics_process(delta):
 	if dashing == false:
 		motion.x = 0
@@ -22,14 +42,13 @@ func _physics_process(delta):
 		moving = false
 	
 	if can_move:
-		_get_input(delta)
+		_get_input()
 	
 	if Input.is_action_just_pressed("space"):
-		if moving && (dashing == false) && can_dash:
+		if moving && dashing == false && $DashCooldown.is_stopped():
 			SPEED *= 5
 			dashing = true
 			can_move = false
-			can_dash = false
 			$DashTimer.start()
 			
 	move_and_slide(motion.normalized() * SPEED)
@@ -37,8 +56,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 
-
-func _get_input(delta):
+#get input for WASD, left and right mouse buttons
+func _get_input():
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
 		moving = true
@@ -56,22 +75,59 @@ func _get_input(delta):
 		moving = true
 	
 	if Input.is_action_just_pressed("shoot"):
-		_shoot_bullet(delta)
-		
-func _shoot_bullet(delta):
+#		only equip ranged if currently equipped melee
+		if current_weapon == 1:
+			current_weapon = 0
+			_equip_ranged()
+		_shoot_bullet()
+	
+	if Input.is_action_just_pressed("melee"):
+#		only equip melee if currently equipped ranged
+		if current_weapon == 0:
+			current_weapon = 1
+			_equip_melee()
+		if $SwordAttack.is_stopped():
+			_melee_attack()
+
+#equip ranged and remove melee if equiped
+func _equip_ranged():
+	if melee != null:
+		melee.queue_free()
+	ranged = gun_load.instance()
+	player.add_child(ranged)
+	ranged.set_global_position(player.get_global_position())
+
+#equip ranged and remove melee if equiped
+func _equip_melee():
+	ranged.queue_free()
+	melee = sword_load.instance()
+	player.add_child(melee)
+	melee.set_global_position(player.get_global_position())
+
+func _shoot_bullet():
 	var bullet = bullet_load.instance()
 	var bullet_rotation = get_angle_to(get_global_mouse_position()) + self.get_rotation()
 	bullet.set_rotation(bullet_rotation)
 	bullet.set_global_position(self.get_global_position())
-	get_parent().add_child(bullet)
+	player.add_child(bullet)
 	var direction_vector = (get_global_mouse_position() - self.get_position()).normalized()
 	bullet.direction = direction_vector
 
+func _melee_attack():
+	print("attack")
+	$SwordAttack.start()
+
+#timer that sets how long and how fast you dash
 func _on_DashTimer_timeout():
 	SPEED = 300
 	can_move = true
 	dashing = false
 	$DashCooldown.start()
 
+#timer for a cooldown after you dash (can't spam)
 func _on_DashCooldown_timeout():
-	can_dash = true
+	$DashCooldown.stop()
+
+#sword attack cooldown
+func _on_SwordAttack_timeout():
+	$SwordAttack.stop()
