ae269e0d2b8a2af0b77f4a28c365e0ae2653e0e7
Kipras <kiprasjanuska0@gmail.com>
2019-11-01 18:19:47 -0500

    state machine basics 2

--- Scripts/StateMachine.gd
+++ Scripts/StateMachine.gd
@@ -1,13 +1,39 @@
 extends Node
+class_name StateMachine
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var state = null setget set_state
+var previous_state = null
+var states = {}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var parent = get_parent()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if state != null:
+		_state_logic(delta)
+		var transition = _get_transtion(delta)
+		if transition != null:
+			set_state(transition)
+
+func _state_logic(delta):
+	pass
+
+func _get_transtion(delta):
+	return null
+
+func _enter_state(new_state, old_state):
+	pass
+
+func _exit_state(old_state, new_state):
+	pass
+
+func set_state(new_state):
+	previous_state = state
+	state = new_state
+	
+	if previous_state != null:
+		_exit_state(previous_state, new_state)
+	if new_state != null:
+		_enter_state(new_state, previous_state)
+
+func add_state(state_name):
+	states[state_name] = states.size()
