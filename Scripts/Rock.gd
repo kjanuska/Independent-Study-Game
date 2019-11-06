2ddce66f1de48aaed52ec25bd31108000a9362c0
Kipras <kiprasjanuska0@gmail.com>
2019-11-05 19:57:55 -0600

    level

--- Scripts/Rock.gd
+++ Scripts/Rock.gd
@@ -1,4 +1,11 @@
 extends Area2D
 
+var health = 3
+
 func take_damage():
 	print("ouch")
+	health -= 1
+	get_parent().get_node("AnimationPlayer").play("hurt")
+	if health == 0:
+		get_parent().queue_free()
+
