01de75dfed47fe51fa1e3266a6dff97f5064561d
Kipras <SkippyKippy@users.noreply.github.com>
2019-12-09 14:29:09 -0600

    added walker features and started working on enemy

--- Scripts/Characters/MrWalker.gd
+++ Scripts/Characters/MrWalker.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 var overlapping = false
 
 func _ready():
+	$Text/TextInterfaceEngine.connect("buff_end", self, "stopped_talking")
 	$AnimationPlayer.play("idle")
+	$ExclamationAnimation.play("ready")
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
@@ -16,5 +18,10 @@ func _on_Area2D_body_exited(body):
 
 func _input(_event):
 	if Input.is_action_pressed("Interact") && overlapping:
+		$AnimationPlayer.play("talk")
+		$ExclamationMark.hide()
 		PlayerVar.player.camera_zoom(0.5, 0.5)
 		$Text.interact()
+
+func stopped_talking():
+	$AnimationPlayer.play("idle")
--- Scripts/Characters/WalkerText.gd
+++ Scripts/Characters/WalkerText.gd
@@ -4,14 +4,17 @@ onready var tie = $TextInterfaceEngine
 
 func ready():
 	tie.reset()
-	tie.set_color("red")
+	tie.buff_text("!")
+	tie.set_state(tie.STATE_OUTPUT)
+	tie.set_color(Color(255, 0, 0))
 	tie.set_size(11)
+
 func interact():
 	tie.reset()
-	tie.set_color("black")
+	tie.set_color(Color(0, 0, 0))
 	tie.set_size(11)
 	tie.buff_text("Hey ", 0.1)
-	tie.buff_text("bro\n", 0.4)
+	tie.buff_text("brrrooo\n", 0.9)
 	tie.buff_silence(1)
 	tie.buff_text("I'm Mr. Walker", 0.05)
 	tie.set_state(tie.STATE_OUTPUT)
