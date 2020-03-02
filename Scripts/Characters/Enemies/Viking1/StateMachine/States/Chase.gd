837cfdc7b43cc8ebc89974906628caf53589249b
Kipras <kiprasjanuska0@gmail.com>
2020-03-01 21:51:43 -0600

    path

--- Scripts/Characters/Enemies/Viking1/StateMachine/States/Chase.gd
+++ Scripts/Characters/Enemies/Viking1/StateMachine/States/Chase.gd
@@ -4,16 +4,20 @@ export(int) var SPEED = 200
 var direction
 
 func enter():
-	pass
+	owner.chasing = true
 
 func update(_delta):
-	direction = Vector2(PlayerVar.player.position.x - owner.position.x, PlayerVar.player.position.y - owner.position.y)
-	.move(SPEED, direction)
+#	direction = Vector2(PlayerVar.player.position.x - owner.position.x, PlayerVar.player.position.y - owner.position.y)
+#	.move(SPEED, direction)
+	var move_distance = SPEED * _delta
+	pass
 
 func _on_StopChaseArea_body_exited(body):
 	if body.get_name() == "Player":
+		owner.chasing = false
 		emit_signal("finished", "idle")
 
 func _on_AttackRange_body_entered(body):
 	if body.get_name() == "Player":
+		owner.chasing = false
 		emit_signal("finished", "attack")
