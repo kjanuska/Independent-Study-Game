5f83fef375979611ca2fb877da8d0e736427ca40
Kipras <kiprasjanuska0@gmail.com>
2020-04-28 16:26:37 -0500

    random

--- Scripts/Weapons/Ranged/ProjectileSpawner.gd
+++ Scripts/Weapons/Ranged/ProjectileSpawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 #store preloaded scene as var
-var arrow_load = preload("res://Weapons/Ranged/Bow/Arrow.tscn")
-var bullet_load = preload("res://Weapons/Ranged/Gun/Bullet.tscn")
+var arrow_load = preload("res://Scenes/Weapons/Ranged/Bow/Arrow.tscn")
+var bullet_load = preload("res://Scenes/Weapons/Ranged/Gun/Bullet.tscn")
 
