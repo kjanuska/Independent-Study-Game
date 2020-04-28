14389d6fdecf0287552155bf48f544279e88bc99
Kipras <kiprasjanuska0@gmail.com>
2020-04-28 14:36:59 -0500

    shooting improvements
    shooting is now better as the enemy shoots only when they see you. if they don't see you then they will start chasing again until they do. removed area2d node for detecting attack range and instead use ray distances

--- Scripts/Characters/Enemies/Viking1/StateMachine/States/Attack.gd
+++ Scripts/Characters/Enemies/Viking1/StateMachine/States/Attack.gd
@@ -3,20 +3,37 @@ extends "../Motion.gd"
 var ammo = preload("res://Scenes/Weapons/Ranged/Bow/Arrow.tscn")
 var can_shoot = true
 
+var aim_rotation
+
+var distance
+
 func enter():
 	pass
 
 func update(_delta):
+	aim()
 	shoot()
+
+func aim():
+	var aim = enemy.get_node("AimSights")
+	aim.cast_to = (enemy.target.position - enemy.position)
+	aim.force_raycast_update()
+
+	if !aim.is_colliding():
+			aim_rotation = aim.cast_to.normalized()
+			distance = enemy.get_global_position().distance_to(PlayerVar.player.get_global_position())
+
+	else:
+		can_shoot = false
+		emit_signal("finished", "chase")
 
 func shoot():
 	if can_shoot:
 		var projectile = ammo.instance()
-		var projectile_rotation = owner.get_angle_to_player()
-		projectile.set_rotation(projectile_rotation)
+		projectile.set_rotation(aim_rotation.angle())
 		projectile.set_global_position(owner.get_global_position())
 		owner.call_deferred("add_child", projectile)
-		var direction_vector = Vector2(PlayerVar.player.position.x - owner.position.x, PlayerVar.player.position.y - owner.position.y).normalized()
+		var direction_vector = aim_rotation
 		projectile.direction = direction_vector
 		projectile.set_speed(500)
 		can_shoot = false
--- Scripts/Characters/Enemies/Viking1/Viking.gd
+++ Scripts/Characters/Enemies/Viking1/Viking.gd
@@ -5,9 +5,5 @@ var target
 func get_angle_to_player():
 	return get_angle_to(PlayerVar.player.get_global_position()) + self.get_rotation()
 
-func _on_FindArea_body_entered(body):
-	if body.get_name() == "Player":
-		SignalManager.emit_signal("player_found")
-
 func _physics_process(delta):
 	target = PlayerVar.player
