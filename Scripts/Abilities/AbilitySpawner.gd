2ed5069f27795a295e2446060d3d8db17dc23f5b
Kipras <kiprasjanuska0@gmail.com>
2020-04-28 16:27:47 -0500

    Revert "random"
    This reverts commit 5f83fef375979611ca2fb877da8d0e736427ca40.

--- Scripts/Abilities/AbilitySpawner.gd
+++ Scripts/Abilities/AbilitySpawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 #store preloaded scene as var
-var shield_load = preload("res://Scenes/Abilities/Shield.tscn")
-var freeze_load = preload("res://Scenes/Abilities/Freeze.tscn")
+var shield_load = preload("res://Abilities/Shield.tscn")
+var freeze_load = preload("res://Abilities/Freeze.tscn")
 
