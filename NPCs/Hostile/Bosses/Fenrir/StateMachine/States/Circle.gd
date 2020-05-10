a542e9be0dd586ab9f53a425c84acdaf478c0053
Kipras <kiprasjanuska0@gmail.com>
2020-05-09 23:31:33 -0500

    follow circle

--- NPCs/Hostile/Bosses/Fenrir/StateMachine/States/Circle.gd
+++ NPCs/Hostile/Bosses/Fenrir/StateMachine/States/Circle.gd
@@ -1,7 +1,41 @@
 extends "../Motion.gd"
 
+var count = 20
+var radius = 150
+var circle
+
+var angle_step = 2.0 * PI / count
+
+var angle = 0
+
+export(int) var SPEED = 200
+
+var circle_path = []
+
+onready var path = preload("res://NPCs/Hostile/Bosses/Fenrir/CirclePath.tscn")
+
 func enter():
-	pass
+	circle = Game.level.get_node("circle")
+	circle.position = fenrir.position
+	for i in range(0, count):
+		var direction = Vector2(cos(angle), sin(angle))
+		var pos = circle.position + direction * radius
+		
+		var node = path.instance()
+		node.set_position(pos)
+		circle.add_child(node)
+		circle_path.push_front(node)
+		angle += angle_step
 
 func update(_delta):
-	pass
+	direction = (circle_path[0].position - fenrir.position).normalized()
+	.move(SPEED, direction)
+	if fenrir.position == circle_path[0].position:
+		circle_path.pop_front()
+	
+
+
+
+
+
+
